1733b69ff1f7f0d32c651e5c6f32d3720115a9db
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2022-11-29 13:02:48 +0400

    Fix build script issue

--- liquidation-bot/bot/build.sh
+++ liquidation-bot/bot/build.sh
@@ -3,7 +3,7 @@ echo "0. all"
 echo "1. shared"
 echo "2. worker"
 echo "3. position-manager"
-echo -n "Please choose a word [0,1,2 or 3]? "
+echo -n "Please choose an option [0,1,2 or 3]? "
 read oper
 
 if [ $oper -eq 1 ] || [ $oper -eq 0 ]
@@ -13,6 +13,7 @@ then
     docker build -t fathombot/shared-base:latest .
     # docker tag fathombot/shared-base:latest intothefathom/fathom-bot-shared:v0.0.1
     echo 'Base image created.'
+    cd ..
 fi
 
 if [ $oper -eq 2 ]  || [ $oper -eq 0 ]
@@ -22,6 +23,7 @@ then
     docker build -t fathombot/worker:latest .
     # docker tag fathombot/worker:latest intothefathom/fathom-bot-worker:v0.0.1
     echo 'worker image created.'
+    cd ..
 fi
 
 if [ $oper -eq 3 ]  || [ $oper -eq 0 ]
@@ -31,4 +33,5 @@ then
     docker build -t fathombot/position-manager:latest .
     # docker tag fathombot/position-manager:latest intothefathom/fathom-bot-position-manager:v0.0.1
     echo 'worker image created.'
+    cd ..
 fi
