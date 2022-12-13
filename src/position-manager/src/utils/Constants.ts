cb04d23e032427054a2d0fbd182f377ad7218350
ztshort <ztshort@hotmail.com>
2022-12-13 08:43:25 -0600

    Update graph urls

--- src/position-manager/src/utils/Constants.ts
+++ src/position-manager/src/utils/Constants.ts
@@ -1,4 +1,4 @@
 export class Constants{
-    public static readonly  GRAPH_URL = "https://graph.composer.live/subgraphs/name/fathomapp-subgraph/graphql"
-    public static readonly  GRAPH_HEALTH_URL = "https://graph.composer.live/graphql/playground"
+    public static readonly  GRAPH_URL = "https://graph.composer.live/subgraphs/name/fathomapp-subgraph"
+    public static readonly  GRAPH_HEALTH_URL = "https://graph.composer.live/graphql"
 }
