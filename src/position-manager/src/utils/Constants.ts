8fc3cf7e7060c72b6a4542e28fbcab1c11ebd267
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2023-01-03 15:24:33 +0400

    Reverted the subgraph urls

--- src/position-manager/src/utils/Constants.ts
+++ src/position-manager/src/utils/Constants.ts
@@ -1,7 +1,12 @@
 export class Constants{
     public static readonly  MAX_POSITION_PER_QUERY = 100;
-    public static readonly  GRAPH_URL = "http://159.223.112.169:8000/subgraphs/name/fathomapp-subgraph"
-    public static readonly  GRAPH_HEALTH_URL = "http://159.223.112.169:8030/graphql"
+    //FOR test subgraph
+    // public static readonly  GRAPH_URL = "http://159.223.112.169:8000/subgraphs/name/fathomapp-subgraph"
+    // public static readonly  GRAPH_HEALTH_URL = "http://159.223.112.169:8030/graphql"
+
+    //FOR Apothem subgraph
+    public static readonly  GRAPH_URL = "https://graph.composer.live/subgraphs/name/fathomapp-subgraph"
+    public static readonly  GRAPH_HEALTH_URL = "https://graph.composer.live/graphql"
 
     public static readonly  WXDC_COLLATRAL_ID = "0x5758444300000000000000000000000000000000000000000000000000000000"
 }
