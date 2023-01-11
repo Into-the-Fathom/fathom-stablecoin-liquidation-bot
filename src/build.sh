4c23af49d510ecab3ea2ba9c83de652ca850cc86
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2023-01-11 16:52:10 +0400

    Change the repository name in docker images

--- src/build.sh
+++ src/build.sh
@@ -16,7 +16,7 @@ if [ $oper -eq 1 ] || [ $oper -eq 0 ]
 then
     cd ./shared
     echo 'Creating base image...'
-    docker build -t tarunshrma/shared-base:latest .
+    docker build -t intothefathom/shared-base:latest .
     # docker tag fathombot/shared-base:latest intothefathom/fathom-bot-shared:v0.0.1
     echo 'Base image created.'
     cd ..
@@ -26,7 +26,7 @@ if [ $oper -eq 2 ]  || [ $oper -eq 0 ]
 then
     cd ./worker
     echo 'Creating worker image...'
-    docker build -t tarunshrma/worker:latest .
+    docker build -t intothefathom/worker:latest .
     # docker tag fathombot/worker:latest intothefathom/fathom-bot-worker:v0.0.1
     echo 'worker image created.'
     cd ..
@@ -36,7 +36,7 @@ if [ $oper -eq 3 ]  || [ $oper -eq 0 ]
 then
     cd ./position-manager
     echo 'Creating worker image...'
-    docker build -t tarunshrma/position-manager:latest .
+    docker build -t intothefathom/position-manager:latest .
     # docker tag fathombot/position-manager:latest intothefathom/fathom-bot-position-manager:v0.0.1
     echo 'worker image created.'
     cd ..
